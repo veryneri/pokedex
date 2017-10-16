202b70ce30ffc7b6d866731267af2a92cdbccc5b
Edson Neri <edson.neri@correo.uia.mx>
2017-10-15 22:18:39 -0500

    add index.js file

---
+++ src/index.js
@@ -0,0 +1,13 @@
+//This is the root component
+import { render } from 'react-dom';
+import React, { Component } from 'react';
+
+class PokeApp extends Component{
+  render() {
+    return <div className="pokeapp">
+      <h1> The Kanto PokeDex! </h1>
+    </div>;
+  }
+}
+
+render(<PokeApp/>, document.getElementById('app'));
